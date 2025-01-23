b762fb261d2129b8106bd882abba97e9e6c73202
Siti Mardhiyah <sitimardhiyah777@gmail.com>
2025-01-23 07:58:11 +0700

    Update ESLint config & fix bugs host value

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,11 +2,21 @@ import globals from 'globals';
 import pluginJs from '@eslint/js';
 import daStyle from 'eslint-config-dicodingacademy';
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   daStyle,
-  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
-  { languageOptions: { globals: globals.browser } },
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      sourceType: 'commonjs', // Menggunakan module CommonJS
+      globals: { ...globals.browser, process: 'readonly' }, // Menambahkan process sebagai global readonly
+    },
+  },
+  {
+    env: {
+      node: true,  // Menambahkan environment Node.js
+      es2021: true, // Menggunakan fitur ES2021
+    },
+  },
   pluginJs.configs.recommended,
 ];
